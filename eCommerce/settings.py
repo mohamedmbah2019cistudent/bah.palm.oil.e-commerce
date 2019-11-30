5125cb06f8a439647d56b59676aee3f1ec1edd74
Mohamed Marifa Bah <mohamedmarifa@gmail.com>
2019-11-30 18:07:25 +0000

    Reinitial commit

--- eCommerce/settings.py
+++ eCommerce/settings.py
@@ -27,9 +27,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [os.environ.get('C9_HOSTNAME'), 
-                '127.0.0.1:8000',
-                'bah-ecommerce.herokuapp.com']
+ALLOWED_HOSTS = [os.environ.get('bah-ecommerce.herokuapp.com')]
 
 
 # Application definition
